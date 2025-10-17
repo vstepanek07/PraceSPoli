f0b81fdc6d3e56bd91fe814d66f4e8a1c808fe3b
Václav Štěpánek <2023.stepanek@skolynome.cz>
2025-10-17 10:11:59 +0200

    Přidejte soubory projektu.

---
+++ PraceSPoli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PraceSPoli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
