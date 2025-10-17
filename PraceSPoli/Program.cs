12a864373d7d800da0272ea523ca23160961c1e8
Václav Štěpánek <2023.stepanek@skolynome.cz>
2025-10-17 10:47:46 +0200

    414564561473121336

--- PraceSPoli/Program.cs
+++ PraceSPoli/Program.cs
@@ -8,7 +8,7 @@
             Console.WriteLine("Cau toto je test githubu");
             Console.WriteLine("cau");
             Console.WriteLine("test test test");
-            Console.WriteLine("test 2");sfaf
+            Console.WriteLine("test 2");sfafdsadsadasdsadsada8794864548674159
         }
     }
 }
