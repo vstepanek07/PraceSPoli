862263fb187a26a8e03ddcfc35102f33c0f2a6dc
Václav Štěpánek <2023.stepanek@skolynome.cz>
2025-10-17 10:40:24 +0200

    1

--- PraceSPoli/Program.cs
+++ PraceSPoli/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Cau toto je test githubu");
-            Console.WriteLine("cau"); Console.WriteLine("fsafdsffdffsfdsfdfdddddfdfdfdsdfsfs");
+            Console.WriteLine("Cau toto je test githubu");guziuzghzáhhhío
+            Console.WriteLine("cau"); Console.WriteLine("fsafdsffdffsfdsfdfdd555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555dddfdfdfdsdfsfs");
         }
     }
 }
