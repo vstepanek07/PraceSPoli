ab688ae4144a94253859dab22f4c5d700cff8db9
Václav Štěpánek <2023.stepanek@skolynome.cz>
2025-10-17 10:25:49 +0200

    Pridan test

--- PraceSPoli/Program.cs
+++ PraceSPoli/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Cau toto je test githubu");
-            Console.WriteLine("cau");
+            Console.WriteLine("cau"); Console.WriteLine("fsafdsfsdfsfs");
         }
     }
 }
