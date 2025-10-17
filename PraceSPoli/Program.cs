5e3f8d02870c651b2bb30797f534e300bca17137
Václav Štěpánek <2023.stepanek@skolynome.cz>
2025-10-17 11:17:27 +0200

    Odstranění výstupů a přidání vstupu pro čísla
    V kódu byly odstraněny všechny výstupy na konzoli.
    Byly přidány proměnné `cislo1`, `cislo2` a `cislo3` pro uchování čísel zadaných uživateli.
    Program nyní žádá o zadání prvního a druhého čísla a kontroluje správnost třetího čísla.

--- PraceSPoli/Program.cs
+++ PraceSPoli/Program.cs
@@ -4,11 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Cau toto je test githubu");
-            Console.WriteLine("cau");
-            Console.WriteLine("test test test");
-            Console.WriteLine("test 2");sfafdsadsadasdsadsada8794864548674159
+            int cislo1;
+            int cislo2;
+            int cislo3;
+
+            Console.WriteLine("Zadejte první číslo:");
+            cislo1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadejte druhé číslo:");
+            cislo2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Zadejte třetí číslo:");
+            //cislo3 = int.Parse(Console.ReadLine());
+
+            if (int.TryParse(Console.ReadLine(), out cislo3) == false)
+            {
+                Console.WriteLine("Třetí číslo bylo zadáno špatně.");
+            }
+            else
+            {
+                
+            }
+
+
+
+
+
         }
     }
 }
