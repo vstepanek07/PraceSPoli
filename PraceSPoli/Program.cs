cdb32c74558976e53738748aab9bf2fe41fa45ca
Václav Štěpánek <2023.stepanek@skolynome.cz>
2025-10-30 11:26:23 +0100

    zatim dobrý

--- PraceSPoli/Program.cs
+++ PraceSPoli/Program.cs
@@ -1,11 +1,10 @@
-﻿using System.Security;
-
-namespace PraceSPoli_3SC
+﻿namespace PraceSPoli_3SC
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             //zalozit 3 x promennou pro ulozeni cisla
             //pres consoli nacist hodnoty do promennych
             //vytvorit funkci co spocita prumer a vrati ho
@@ -14,72 +13,98 @@ namespace PraceSPoli_3SC
             int cislo1;
             int cislo2;
             int cislo3 = 0;
-            //toto funguje jen na 3 cisla
-            //univerzalne na 100 cisel
-            //zakladam pole (array)
+            // toto funguje jen pro tri cisla 
+            // universalne na 100 cisel
+            // zakladam pole (array)
             int velikostPole = 100;
+            //int[] cisla = new int[velikostPole];
+
+
+            //Console.WriteLine(cisla[0]);
+            // Console.WriteLine(cisla[5]); // nic jsem tam nezadal ale pri vzniku se zada do 
+            //Console.WriteLine(cisla[100]);//spatne pozice s indexem 100 neexistuje 
+            //Console.WriteLine(cisla);// nespadne, ale napíše systemovy typ promenny 
+
+
+            Console.WriteLine("kolik budes zadavat cisel?");
+            velikostPole = int.Parse(Console.ReadLine());
             int[] cisla = new int[velikostPole];
             //cisla[0] = 1;
             //cisla[1] = 5;
 
-            //Console.WriteLine(cisla[0]);
-            //Console.WriteLine(cisla[5]);
-            //Console.WriteLine(cisla[100]);// je sptane protoze pozice 100 neexistuje
-            //Console.WriteLine(cisla);// nespadne, napise jen systemovy typ prgramu
-            Console.WriteLine("kolik čísel chceš zadat?");
-            velikostPole = int.Parse(Console.Readline());
-            int[] cisla = new int[velikostPole];
-
             int currentIndex = 0;
 
-            while (SecureString < velikostPole)
+            while (currentIndex < velikostPole)
             {
-                Console.WriteLine($"zadej číslo {currentIndex + 1})
-                int tempCislo = int.Parse(Console.ReadLine());
+                Console.WriteLine($"zadej cislo {currentIndex + 1}");
+                int tempcislo = int.Parse(Console.ReadLine());
+                cisla[currentIndex] = tempcislo;
 
 
 
 
-                currentIndex++;
-                C
+
+                currentIndex++;//++currentIndex = currentindex +1
             }
 
+            Console.WriteLine(LepsiVypocetPrumeru(cisla));
 
-            Console.WriteLine("Zadej cislo 1");
-            cislo1 = int.Parse(Console.ReadLine());
-
-            //to same jako si rozdelit na:
-            //string text = Console.ReadLine();
-            //int.Parse(text);
-
-            Console.WriteLine("Zadej cislo 2");
-            cislo2 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Zadej cislo 3");
-
-            bool jdeOCislo = false;
-
-            while (jdeOCislo == false)
+            double LepsiVypocetPrumeru(int[] cislaIn)
             {
-                if (int.TryParse(Console.ReadLine(), out cislo3) == false)
-                {
-                    Console.WriteLine("Zadana spatna hodnota, nejde o cislo!");
-                }
-                else
-                {
-                    jdeOCislo = true;
-                }
+                double prumer = 0;
+                int soucetCisel = 0;
 
+                int velikosPole = cislaIn.Length; // zjistí velikost pole ktera posle uzivatel funkce 
+                int currentIndex = 0;
+
+                while (currentIndex < velikosPole)
+                {
+                    soucetCisel = soucetCisel + cislaIn[currentIndex];
+                    currentIndex++;
+                }
+                prumer = soucetCisel / (double)velikostPole;
+
+                return prumer;
+              
             }
+            
+
+
+            //Console.WriteLine("Zadej cislo 1");
+            //cislo1 = int.Parse(Console.ReadLine());
+
+            ////to same jako si rozdelit na:
+            ////string text = Console.ReadLine();
+            ////int.Parse(text);
+
+            //Console.WriteLine("Zadej cislo 2");
+            //cislo2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Zadej cislo 3");
+
+            //bool jdeOCislo = false;
+
+            //while (jdeOCislo == false)
+            //{
+            //    if (int.TryParse(Console.ReadLine(), out cislo3) == false)
+            //    {
+            //        Console.WriteLine("Zadana spatna hodnota, nejde o cislo!");
+            //    }
+            //    else
+            //    {
+            //        jdeOCislo = true;
+            //    }
+
+            //}
             //kdyz JdeOCislo bude True (ANO) pak kod pokracuje zde, jinak se opakuje ve smycce
 
 
 
-            double prumer = VypocetPrumeru(cislo1, cislo2, cislo3);
-            int max = GetMaxValue(cislo1, cislo2, cislo3);
-            Console.WriteLine($"Prumer cisel je {prumer}");
-            Console.WriteLine($"Maximum cisel je {max}");
-            NapisCislo1();
+            //double prumer = VypocetPrumeru(cislo1, cislo2, cislo3);
+            //int max = GetMaxValue(cislo1, cislo2, cislo3);
+            //Console.WriteLine($"Prumer cisel je {prumer}");
+            //Console.WriteLine($"Maximum cisel je {max}");
+            //NapisCislo1();
 
             double VypocetPrumeru(int n1, int n2, int n3)
             {
@@ -98,7 +123,7 @@ namespace PraceSPoli_3SC
             int GetMaxValue(int n1, int n2, int n3)
             {
                 int max = n1;
-                //
+
                 if (n2 > max)
                 {
                     max = n2;
@@ -109,9 +134,6 @@ namespace PraceSPoli_3SC
                 }
                 return max;
             }
-
-
-
 
 
         }
